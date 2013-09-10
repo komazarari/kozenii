8a817f75154b3d8a6c3ecfb2a7a06e5603459edf
takuto.komazaki <takuto.komazaki@gree.net>
2013-09-10 19:20:52 +0900

    initialized by 'rails new kozenii -T'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kozenii::Application.config.secret_key_base = '8c76c6f7f78cb68cbdd04704bd6b07e44ece98908601409e98f35194199cecb8d9a2a9a7c82a23544622344a9d50af15a043164548a902262987a42b8e7bafa3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kozenii::Application.config.session_store :cookie_store, key: '_kozenii_session'
