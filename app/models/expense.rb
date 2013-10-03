b91d33cb4e5c881a5fb681e27ac44f5c95df0640
komazarari <komazarari@gmail.com>
2013-10-03 14:25:55 +0900

    Validate expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,5 @@
 class Expense < ActiveRecord::Base
+  validates :used_date, presence: true
   validates :amount, presence: true, numericality: { only_integer: true }
+  validates :spend_for, presence: true
 end
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Expense do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { expect(FactoryGirl.build(:expense)).to be_valid }
+
+  it "requires a used_date" do
+    expect(FactoryGirl.build(:expense, used_date: "")).not_to be_valid
+  end
+  it "requires a amount" do
+    expect(FactoryGirl.build(:expense, amount: "")).not_to be_valid
+  end
+  it "requires a spend_for" do
+    expect(FactoryGirl.build(:expense, spend_for: "")).not_to be_valid
+  end
 end
