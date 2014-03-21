ba773db733847a22599baded5ae1f6d223fb2d2e
takuto.komazaki <komazarari@gmail.com>
2014-03-21 15:41:43 +0900

    let skeletons of specs

---
+++ spec/factories/incomes.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :income do
+    obtained_date "2014-03-21"
+    amount 1
+    spend_for "MyText"
+    comment "MyText"
+    valid_req false
+  end
+end
